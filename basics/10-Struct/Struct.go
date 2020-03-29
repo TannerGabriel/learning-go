7eee19ecd6145d81ddee8584d44c360e35b40f90
TannerGabriel <gabrieltanner.code@gmail.com>
2020-03-29 12:11:41 +0200

    Struct basics

---
+++ basics/10-Struct/Struct.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Declaring a Struct
+type Animal struct {
+	name   string
+	weight int
+}
+
+func main() {
+	// Creating an instance of a struct
+	var dog Animal
+	dog.name = "Dog"
+	dog.weight = 40
+	fmt.Println(dog)
+
+	// Creating an instance using struct literate
+	var cat = Animal{name: "Cat", weight: 5}
+	fmt.Println(cat)
+
+	// Creating an instance using the new keyword
+	var bird = new(Animal)
+	bird.name = "Bird"
+	bird.weight = 1
+	fmt.Println(bird)
+
+	// Creating an instance using the pointer address operator
+	var monkey = &Animal{name: "Monkey", weight: 10}
+	fmt.Println(monkey)
+}
