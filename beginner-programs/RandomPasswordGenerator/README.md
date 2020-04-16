cfd920fec4ea685fd83b9973066d52d2866afc25
TannerGabriel <gabrieltanner.code@gmail.com>
2020-04-16 11:08:14 +0200

    Random password generator

---
+++ beginner-programs/RandomPasswordGenerator/README.md
@@ -0,0 +1,7 @@
+# Random Password Generator
+
+Basic Password Generator using the official math/rand package. 
+
+## Implementation
+
+* [Password Generator](https://github.com/TannerGabriel/learning-go/blob/master/beginner-programs/RandomPasswordGenerator/generator.go)
---
+++ beginner-programs/RandomPasswordGenerator/generator.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generatePassword(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	digits := "0123456789"
+	specials := "~=+%^*/()[]{}/!@#$?|"
+	all := "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		digits + specials
+
+	buf := make([]byte, length)
+	buf[0] = digits[rand.Intn(len(digits))]
+	buf[1] = specials[rand.Intn(len(specials))]
+
+	for i := 2; i < length; i++ {
+		buf[i] = all[rand.Intn(len(all))]
+	}
+	rand.Shuffle(len(buf), func(i, j int) {
+		buf[i], buf[j] = buf[j], buf[i]
+	})
+
+	str := string(buf)
+	return str
+}
+
+func main() {
+	password := generatePassword(10)
+	fmt.Println(password)
+}
