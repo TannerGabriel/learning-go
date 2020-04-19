eed9576223aeb11c123a08dc27787a5e63591a5a
TannerGabriel <gabrieltanner.code@gmail.com>
2020-04-19 16:04:01 +0200

    Saving file when uploaded

--- beginner-programs/FileUpload/main.go
+++ beginner-programs/FileUpload/main.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 	"text/template"
 )
 
-//Compile templates on start
-var templates = template.Must(template.ParseFiles("beginner-programs/FileUpload/public/upload.html"))
+// Compile templates on start of the application
+var templates = template.Must(template.ParseFiles("public/upload.html"))
 
-//Display the named template
-func display(w http.ResponseWriter, tmpl string, data interface{}) {
-	templates.ExecuteTemplate(w, tmpl+".html", data)
+// Display the named template
+func display(w http.ResponseWriter, page string, data interface{}) {
+	templates.ExecuteTemplate(w, page+".html", data)
 }
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("File Upload Endpoint Hit")
-
 	// Maximum upload of 10 MB files
 	r.ParseMultipartForm(10 << 20)
 
@@ -34,38 +33,34 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("File Size: %+v\n", handler.Size)
 	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-	// Create a temporary file within our temp-images directory
-	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	// Create file
+	dst, err := os.Create(handler.Filename)
+	defer dst.Close()
 	if err != nil {
-		fmt.Println(err)
-	}
-	defer tempFile.Close()
-
-	// Read all of the contents of our uploaded file into a byte array
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	// Write the byte array to our temporary file
-	tempFile.Write(fileBytes)
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	//GET displays the upload form.
 	case "GET":
 		display(w, "upload", nil)
-
-	//POST takes the uploaded file(s) and saves it to disk.
 	case "POST":
 		uploadFile(w, r)
 	}
 }
 
 func main() {
+	// Upload route
 	http.HandleFunc("/upload", uploadHandler)
 
 	//Listen on port 8080
