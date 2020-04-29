d35a236b0f3379d849271fa0a6c2d9141bac50df
TannerGabriel <gabrieltanner.code@gmail.com>
2020-04-29 10:23:43 +0200

    Radixsort implementation

---
+++ algorithms/sorting/RadixSort/radixsort_test.go
@@ -0,0 +1,29 @@
+package RadixSort
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+)
+
+func TestMergeSort(t *testing.T) {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	array1 := make([]int, random.Intn(100-10)+10)
+
+	for i := range array1 {
+		array1[i] = random.Intn(100)
+	}
+	array2 := make(sort.IntSlice, len(array1))
+
+	copy(array2, array1)
+	n := len(array1); 
+    radixsort(array1, n); 
+	array2.Sort()
+
+	for i := range array1 {
+		if array1[i] != array2[i] {
+			t.Fail()
+		}
+	}
+}
