af59f901bbbf45acb53d49e65c0c22104ac7252f
TannerGabriel <gabrieltanner.code@gmail.com>
2020-05-03 16:12:28 +0200

    Sieve of Eratosthenes implementation

---
+++ algorithms/math/SieveOfEratosthenes/sieveOfEratosthenes.go
@@ -0,0 +1,34 @@
+package SieveOfEratosthenes
+
+func sieveOfEratosthenes(maxNumber int) []int {
+	isPrime := make([]bool, maxNumber+1)
+
+	// Initialize array with true values
+	for i := 0; i < maxNumber+1; i++ {
+		isPrime[i] = true
+	}
+
+	isPrime[0] = false
+	isPrime[1] = false
+
+	var primes []int
+
+	// Loop until max number
+	for num := 2; num <= maxNumber; num ++ {
+		if isPrime[num] == true {
+			// Save prime number in array
+			primes = append(primes, num)
+
+			// Start marking multiples of p from p * p, and not from 2 * p because, smaller multiples of p will have already been marked false.
+			nextNum := num * num
+
+			// Mark all multiples of a prime number as false
+			for nextNum <= maxNumber {
+				isPrime[nextNum] = false
+				nextNum += num
+			}
+		}
+	}
+
+	return primes
+}
---
+++ algorithms/math/SieveOfEratosthenes/sieveOfEratosthenes_test.go
@@ -0,0 +1,66 @@
+package SieveOfEratosthenes
+
+import (
+	"testing"
+)
+
+func TestSieveOfEratosthenes(t *testing.T) {
+	t.Run("Prime Numbers until 10", func(t *testing.T) {
+		got := sieveOfEratosthenes(10)
+		want := []int{  
+			2, 3, 5, 7,
+		}
+
+		if len(got) != len(want) {
+			t.Errorf("Different array size then expected")
+		}
+
+		if !Equal(got, want) {
+			t.Errorf("Error")
+		}
+	})
+
+	t.Run("Prime Numbers until 50", func(t *testing.T) {
+		got := sieveOfEratosthenes(50)
+		want := []int{  
+			2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+		}
+
+		if len(got) != len(want) {
+			t.Errorf("Different array size then expected")
+		}
+
+		if !Equal(got, want) {
+			t.Errorf("Error")
+		}
+	})
+
+	t.Run("Prime Numbers until 100", func(t *testing.T) {
+		got := sieveOfEratosthenes(100)
+		want := []int{  
+			2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+		}
+
+		if len(got) != len(want) {
+			t.Errorf("Different array size then expected")
+		}
+
+		if !Equal(got, want) {
+			t.Errorf("Error")
+		}
+	})
+}
+
+func Equal(a, b []int) bool {
+    if len(a) != len(b) {
+        return false
+	}
+	
+    for i, v := range a {
+        if v != b[i] {
+            return false
+        }
+	}
+	
+    return true
+}
