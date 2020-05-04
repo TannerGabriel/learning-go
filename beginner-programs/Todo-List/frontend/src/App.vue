1c4139af9252ea66b148b24f4523dfb26fea4e96
TannerGabriel <gabrieltanner.code@gmail.com>
2020-05-04 21:25:38 +0200

    Added frontend to todo list project

---
+++ beginner-programs/Todo-List/frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'app',
+  components: {
+   Main     
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ beginner-programs/Todo-List/frontend/src/components/Main.vue
@@ -0,0 +1,100 @@
+<template>
+<!-- eslint-disable -->
+  <section>
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+    <h1>TODOS</h1>
+    <input type="text" id="todo-input" placeholder="Add todo...">  
+    <button v-on:click="addTodo">Add todo</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.text" class="flex">
+        <label>
+          <section v-if="todo.status == true">
+            <input type="checkbox" v-on:click="changeStatus(todo._id, true)" checked>
+          </section>
+          <section v-else>
+            <input type="checkbox" v-on:click="changeStatus(todo._id, false)">
+          </section>
+
+          <span>&nbsp</span>
+        </label>
+        <span>{{ todo.task }}</span>
+        <div class="space"></div>
+        <button class="is-error padding" v-on:click="removeTodo(todo._id)">X</button>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+/* eslint-disable */
+export default {
+  name: 'Main',
+  data: function () {
+    return {
+      todos: [] 
+    }
+  },
+  beforeMount(){
+    this.getTodos()
+  },
+  methods: {
+    addTodo: async function () {
+      const element = document.getElementById("todo-input");
+      axios.post('http://localhost:3000/todo', {
+        task: element.value
+      })   
+      this.todos.push({task: element.value})  
+    },
+    removeTodo: function(id) {
+      axios.delete(`http://localhost:3000/todo/deleteTask/${id}`, () => {})   
+      const index = this.todos.findIndex(x => x._id === id)
+      this.todos.splice(index, 1);
+    },
+    changeStatus: function(id, status) {
+      if(status == true) {
+        axios.put(`http://localhost:3000/todo/undoTask/${id}`, () => {}) 
+      } else {
+        axios.put(`http://localhost:3000/todo/${id}`, () => {}) 
+      }
+    },
+    getTodos: async function(){
+      const todos = await axios.get('http://localhost:3000/todo')
+      todos.data.forEach(element => {
+        this.todos.push(element)
+      });
+    }
+  },
+}
+</script>
+
+<style>
+html, body, pre, code, kbd, samp {
+  font-family: "Press Start 2P";
+}
+body {
+  background: #20262E;
+  padding: 20px;
+}
+#app {
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  transition: all 0.2s;
+}
+li {
+  margin: 8px 0;
+}
+del {
+  color: rgba(0, 0, 0, 0.3);
+}
+.padding {
+  padding: 1px 7px 2px;
+}
+.flex {
+  display: flex;
+}
+.space {
+  flex-grow: 1;
+}
+</style>
