f092f388306e8b59a47103d5da83ef6446b921ed
TannerGabriel <gabrieltanner.code@gmail.com>
2020-05-07 10:27:00 +0200

    Webscraping documentation

---
+++ beginner-programs/WebScraper/README.md
@@ -0,0 +1,21 @@
+# Web Scraper
+
+Simple webscraping example using the Colly libary.
+
+## Requirements
+
+- Local golang installation
+
+## Getting started
+
+Installing the dependencies:
+
+```bash
+go get github.com/gocolly/colly
+```
+
+Running the application:
+
+```bash
+go run scraper.go
+```
--- beginner-programs/WebScraper/scraper.go
+++ beginner-programs/WebScraper/scraper.go
@@ -1,38 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "github.com/gocolly/colly"
+	"fmt"
+	"time"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
-    c := colly.NewCollector(
-        colly.AllowedDomains("gabrieltanner.org"),
-    )
+	c := colly.NewCollector(
+		colly.AllowedDomains("gabrieltanner.org"),
+	)
 
-    // Callback for when a scraped page contains an article heading
-    c.OnHTML(".article-heading", func(e *colly.HTMLElement) {   
-		fmt.Println("Article heading: ", e.Text)     
-    })
+	// Callback for when a scraped page contains an article container
+	c.OnHTML(".article-container", func(e *colly.HTMLElement) {
+		fmt.Println("Article heading: ", e.DOM.Find("h1").Text())
+	})
 
-    // Callback for links on scraped pages
-    c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-        // Extract the linked URLs from the anchor tag
-        link := e.Attr("href")
-        // Have your crawler visit the linked URL
-        c.Visit(e.Request.AbsoluteURL(link))
-    })
+	// Callback for links on scraped pages
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// Extract the linked URLs from the anchor tag
+		link := e.Attr("href")
+		// Have your crawler visit the linked URL
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Callback getting called when the scraping process is finished
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished scraping!")
+	})
 
 	// Set a rate limit for the crawler
-    c.Limit(&colly.LimitRule{
-        DomainGlob:  "*",
-        RandomDelay: 1 * time.Second,
-    })
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		RandomDelay: 1 * time.Second,
+	})
 
-    c.OnRequest(func(r *colly.Request) {
-        fmt.Println("Visiting", r.URL.String())
-    })
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
 
-    c.Visit("https://gabrieltanner.org")
+	c.Visit("https://gabrieltanner.org")
 }
