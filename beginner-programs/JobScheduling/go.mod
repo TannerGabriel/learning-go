3fdcd424aea00964dfde258b01ce39b317081878
TannerGabriel <gabrieltanner.code@gmail.com>
2020-05-12 08:16:36 +0200

    JobScheduling using cron

---
+++ beginner-programs/JobScheduling/go.mod
@@ -0,0 +1,5 @@
+module github.com/tannergabriel/learning-go/beginner-programs/JobScheduling
+
+go 1.13
+
+require github.com/robfig/cron/v3 v3.0.0 // indirect
--- beginner-programs/JobScheduling/scheduler.go
+++ beginner-programs/JobScheduling/scheduler.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/robfig/cron/v3"
 )
 
 func main() {
+
 	c := cron.New()
-	c.AddFunc("@every 0h1m", func() { sayHelloTo("Someone!") })
+
+	// Funcs are automatically invoked in their own goroutine, asynchronously
+	c.AddFunc("0 30 * * * *", func() { fmt.Println("Every hour on the half hour") })
+	c.AddFunc("TZ=America/New_York 30 04 * * * *", func() { fmt.Println("Runs at 04:30 New York time every day") })
+	c.AddFunc("@hourly", func() { fmt.Println("Runs every hour") })
+	c.AddFunc("@every 0h0m1s", func() { sayHelloTo("Someone!") })
 	c.Start()
-	time.Sleep(1 * time.Minute)
+
+	// Funcs may also be added to a running Cron
+	c.AddFunc("@daily", func() { fmt.Println("Every day") })
+
+	// Added time to see output
+	time.Sleep(10 * time.Second)
+
+	c.Stop() // Stop the scheduler (does not stop any jobs already running)
+
 }
 
-func sayHelloTo(name string) string {
-	return "Hello " + name
+func sayHelloTo(name string) {
+	fmt.Println("Hello ", name)
 }
