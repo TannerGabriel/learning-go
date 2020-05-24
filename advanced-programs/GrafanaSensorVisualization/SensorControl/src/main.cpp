0a7586c39663a5111c305bcf8c903e2ddd2ace07
TannerGabriel <gabrieltanner.code@gmail.com>
2020-05-24 20:24:12 +0200

    Added MQTT client for sending sensor data

--- advanced-programs/GrafanaSensorVisualization/SensorControl/src/main.cpp
+++ advanced-programs/GrafanaSensorVisualization/SensorControl/src/main.cpp
@@ -1,37 +1,140 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_TMP006.h"
-
-
-// Connect VCC to +3V (its a quieter supply than the 5V supply on an Arduino
-// Gnd -> Gnd
-// SCL connects to the I2C clock pin. On newer boards this is labeled with SCL
-// otherwise, on the Uno, this is A5 on the Mega it is 21 and on the Leonardo/Micro digital 3
-// SDA connects to the I2C data pin. On newer boards this is labeled with SDA
-// otherwise, on the Uno, this is A4 on the Mega it is 20 and on the Leonardo/Micro digital 2
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 Adafruit_TMP006 tmp006;
-//Adafruit_TMP006 tmp006(0x41);  // start with a diferent i2c address!
 
-void setup() { 
+// Update these with values suitable for your network.
+const char* ssid = "ssid";
+const char* password = "password";
+const char* mqtt_server = "10.0.0.22";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(100)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+uint32_t unixtime;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), "mqtt", "")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(9600);
-  Serial.println("Adafruit TMP006 example");
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
-  // you can also use tmp006.begin(TMP006_CFG_1SAMPLE) or 2SAMPLE/4SAMPLE/8SAMPLE to have
-  // lower precision, higher rate sampling. default is TMP006_CFG_16SAMPLE which takes
-  // 4 seconds per reading (16 samples)
   if (! tmp006.begin()) {
     Serial.println("No sensor found");
     while (1);
   }
 
-  Serial.println("Send s to enter sleep mode, or w to wake up.  Measurements are not updated while asleep!");
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(3600);
 }
 
 void loop() {
-  // Check for sleep/wake command.
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
+
   while (Serial.available() > 0) {
     char c = Serial.read();
     if (c == 'w') {
@@ -44,11 +147,20 @@ void loop() {
     }
   }
 
-  // Grab temperature measurements and print them.
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+
+  unixtime = timeClient.getEpochTime() * 1000000000;
+  Serial.println(unixtime);
+
+  // uint32_t unixTime = time.unixtime();
   float objt = tmp006.readObjTempC();
   Serial.print("Object Temperature: "); Serial.print(objt); Serial.println("*C");
   float diet = tmp006.readDieTempC();
   Serial.print("Die Temperature: "); Serial.print(diet); Serial.println("*C");
-   
-  delay(4000); // 4 seconds per reading for 16 samples per reading
+  Serial.print("Timestamp: "); Serial.println(unixtime);
+  snprintf (msg, MSG_BUFFER_SIZE, "weather,location=us-midwest temperature=%ld %ld", objt, unixtime);
+  client.publish("sensors", msg);
+  delay(4000); 
 }
